9212b3248996eb8e3287d13f19d6859edd0d599a
Max Gatchang <maxgatchang96@gmail.com>
2023-07-20 19:11:06 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    //2. c2 = a2+b2. if a right-angled triangle has base 4cm, height 3cm
+    //find the hoeght of the hypotenuse of the triangle
+    //c = square root  root(a2 + b2)
+    val a = 4
+    val b = 3
+    val p = a*a + b*b
+    val x = p.toDouble()
+    print(x.toInt())
+}
---
+++ src/main/kotlin/nullific.kt
@@ -0,0 +1,10 @@
+fun  main(){
+//    println("Enter first number  from keyboard")
+//    val d = readlnOrNull()?.toInt()
+//    print("Enter second number from the keyboard")
+//    val e = readlnOrNull()?.toInt()
+//
+//    val sum = e?.let { d?.plus(it) }
+//    print(sum)
+
+}
